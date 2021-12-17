43ccc3075902ad06512fd2c5245f0eb7345bfa70
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2021-12-17 02:28:30 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package report-Stock;
+
+import java.utils.*;
+
+import java.util.InputMismatchException; 
+/*
+ *  exception to add numbers as char
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * author Nordevelop
+		 * program Stock-Report
+     * segun_proveedor
+     * segun_empleado
+     * segun_zona-de-entrega
+     */
